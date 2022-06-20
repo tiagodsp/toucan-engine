68fcbb06308cb262f37e6f50fbfe6a0f4f62d802
Tiago <tiagodsp93@gmail.com>
2022-06-20 22:08:25 +0200

    Add primitive types

--- src/CoreTypes.h
+++ src/CoreTypes.h
@@ -1,12 +1,37 @@
 #pragma once
 #include "Toucan.h"
 
+// primitive types
+#ifndef __GNUG__
+typedef signed char int8_t;
+typedef signed short int int16_t;
+typedef signed int int32_t;
+typedef signed long long int64_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short int uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long long uint64_t;
+#endif
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 namespace Toucan
 {
 
-template <typename T> using Scope = std::unique_ptr<T>;
-template <typename T> using Ref = std::shared_ptr<T>;
-template <typename T> using Weak = std::weak_ptr<T>;
+template <typename T>
+using Scope = std::unique_ptr<T>;
+template <typename T>
+using Ref = std::shared_ptr<T>;
+template <typename T>
+using Weak = std::weak_ptr<T>;
 
 using HANDLE = void *;
 
